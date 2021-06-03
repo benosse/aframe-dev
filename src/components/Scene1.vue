66a5a431ec3df2160c4acd46beb5269551ddb878
benoit <benoit.zenker@gmail.com>
2021-06-03 11:10:43 +0200

    avant ajout router

---
+++ src/components/Scene1.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>scene 1</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Scene1",
+};
+</script>
+
+
+
+<style scoped>
+</style>
---
+++ src/components/Scene2.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>scene 1</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Scene2",
+};
+</script>
+
+
+
+<style scoped>
+</style>
